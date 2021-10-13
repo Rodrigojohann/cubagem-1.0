8e03e762758d5d85db9bc404a7e96dc14f7fd15a
rodrigo <rodrigo@debian.debian>
2021-10-13 15:39:13 -0300

    server test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include "pclviewer.h"
 #include <boost/asio.hpp>
 
+using namespace boost;
 
 int main ()
 {
-  char* ip = "192.168.140.2";
+    // Step 1. Here we assume that the server application has
+    //already obtained the protocol port number.
+    unsigned short port_num = 3333;
+
+    // Step 2. Create special object of asio::ip::address class
+    // that specifies all IP-addresses available on the host. Note
+    // that here we assume that server works over IPv6 protocol.
+    asio::ip::address ip_address = asio::ip::address_v6::any();
+
+    // Step 3.
+    asio::ip::tcp::endpoint ep(ip_address, port_num);
+
+    // Step 4. The endpoint is created and can be used to
+    // specify the IP addresses and a port number on which
+    // the server application wants to listen for incoming
+    // connections.
+
+    return 0;
+
+
 
   PCLViewer w;
   w.Run(ip);
