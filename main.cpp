4b2516171c85d9e68d87833fd46873babf518f1e
rodrigo <rodrigo@debian.debian>
2021-10-21 10:22:25 -0300

    serialization

--- main.cpp
+++ main.cpp
@@ -1,41 +1,12 @@
 #include "pclviewer.h"
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
-#include <boost/smart_ptr.hpp>
-#include <boost/thread/thread.hpp>
-
+#include <boost/lexical_cast.hpp>
+#include "connection.hpp" // Must come before boost/serialization headers.
+#include <boost/serialization/vector.hpp>
 
 using namespace boost;
 using boost::asio::ip::tcp;
-typedef boost::shared_ptr<tcp::socket> socket_ptr;
-
-
-void session(socket_ptr sock, char* ip)
-{
-  PCLViewer w;
-
-  try
-  {
-    for (;;)
-    {
-      ObjectsData outputdata = w.Run(ip);
-
-      boost::system::error_code error;
-      size_t length = sock->read_some(boost::asio::buffer(outputdata), error);
-      if (error == boost::asio::error::eof)
-        break; // Connection closed cleanly by peer.
-      else if (error)
-        throw boost::system::system_error(error); // Some other error.
-
-      boost::asio::write(*sock, boost::asio::buffer(outputdata, length));
-    }
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception in thread: " << e.what() << "\n";
-  }
-}
-
 
 int main (int argc, char *argv[])
 {
@@ -44,7 +15,6 @@ int main (int argc, char *argv[])
     PCLViewer w;
 //    ObjectsData outputdata;
 /////
-
     try
     {
       boost::asio::io_service io_service;
@@ -55,16 +25,16 @@ int main (int argc, char *argv[])
 
       for (;;)
       {
-        socket_ptr sock(new tcp::socket (io_service));
+        tcp::socket socket(io_service);
         unsigned int nr_points = 0;
 
+
         boost::system::error_code ignored_error;
-        acceptor.accept(*sock);
-        boost::thread t(boost::bind(session, sock, ip));
+        acceptor.accept(socket);
 
-//        ObjectsData outputdata = w.Run(ip);
+        ObjectsData outputdata = w.Run(ip);
 
-//        boost::asio::write (socket, boost::asio::buffer(&outputdata, sizeof(outputdata)), ignored_error);
+        boost::asio::write (socket, boost::asio::buffer(&outputdata, sizeof(outputdata)), ignored_error);
 
 //        boost::asio::write (socket, boost::asio::buffer(outputdata.box1.front(), sizeof(outputdata.box1)));
 //        boost::asio::write (socket, boost::asio::buffer(outputdata.input.front(), sizeof(outputdata.input)));
