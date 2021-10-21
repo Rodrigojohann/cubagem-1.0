9888369a20d457326dbec8cbbe5be5361776fb8f
rodrigo <rodrigo@debian.debian>
2021-10-21 14:34:15 -0300

    test

--- pclviewer.cpp
+++ pclviewer.cpp
@@ -8,7 +8,6 @@ ObjectsData PCLViewer::Run(char* ipaddr){
     Controller c;
     Sensor s;
     ObjectsData outputdata;
-//    PointCloudBuffers::Ptr new_buffers = PointCloudBuffers::Ptr(new PointCloudBuffers);
 
     x1 = x2 = x3 = x4 = x5 = 0.0;
     y1 = y2 = y3 = y4 = y5 = 0.0;
@@ -26,126 +25,126 @@ ObjectsData PCLViewer::Run(char* ipaddr){
 
             cloudnew = s.CamStream(ipaddr, PORT);
 
-//            filteredcloud = c.FilterCloud(cloudnew);
-//            std::tie(unsortedclusters, clustersize) = c.CloudSegmentation(filteredcloud);
+            filteredcloud = c.FilterCloud(cloudnew);
+            std::tie(unsortedclusters, clustersize) = c.CloudSegmentation(filteredcloud);
 
-//            notorientedclusters = c.SortClusters(unsortedclusters, clustersize);
-//            clusters = c.RemoveInclined(filteredcloud, notorientedclusters);
+            notorientedclusters = c.SortClusters(unsortedclusters, clustersize);
+            clusters = c.RemoveInclined(filteredcloud, notorientedclusters);
 
-//            if (clusters.size() > 5)
-//            {
-//                limitcluster = 5;
-//            }
-//            else
-//            {
-//                limitcluster = clusters.size();
-//            }
+            if (clusters.size() > 5)
+            {
+                limitcluster = 5;
+            }
+            else
+            {
+                limitcluster = clusters.size();
+            }
 
 
-//            for (int number=0; number<limitcluster; ++number)
-//            {
-//                segmented_cloud.reset(new pcl::PointCloud<pcl::PointXYZ>);
-//                segmented_cloud->points.resize(clusters[number].indices.size());
+            for (int number=0; number<limitcluster; ++number)
+            {
+                segmented_cloud.reset(new pcl::PointCloud<pcl::PointXYZ>);
+                segmented_cloud->points.resize(clusters[number].indices.size());
 
-//                for(size_t i=0; i<clusters[number].indices.size(); ++i)
-//                {
-//                    segmented_cloud->points[i].x = (*filteredcloud)[clusters[number].indices[i]].x;
-//                    segmented_cloud->points[i].y = (*filteredcloud)[clusters[number].indices[i]].y;
-//                    segmented_cloud->points[i].z = (*filteredcloud)[clusters[number].indices[i]].z;
-//                }
+                for(size_t i=0; i<clusters[number].indices.size(); ++i)
+                {
+                    segmented_cloud->points[i].x = (*filteredcloud)[clusters[number].indices[i]].x;
+                    segmented_cloud->points[i].y = (*filteredcloud)[clusters[number].indices[i]].y;
+                    segmented_cloud->points[i].z = (*filteredcloud)[clusters[number].indices[i]].z;
+                }
 
-//                std::tie(dimensionX, dimensionY, dimensionZ) = c.CalculateDimensions(segmented_cloud);
+                std::tie(dimensionX, dimensionY, dimensionZ) = c.CalculateDimensions(segmented_cloud);
 
-//                if (number == 0)
-//                {
-//                    x1 += dimensionX;
-//                    y1 += dimensionY;
-//                    z1 += dimensionZ;
-//                    outputcloud1 = ConvertCloudtoVector(segmented_cloud);
-//                }
-//                else if (number == 1)
-//                {
-//                    x2 += dimensionX;
-//                    y2 += dimensionY;
-//                    z2 += dimensionZ;
-//                    outputcloud2 = ConvertCloudtoVector(segmented_cloud);
-//                }
-//                else if (number == 2)
-//                {
-//                    x3 += dimensionX;
-//                    y3 += dimensionY;
-//                    z3 += dimensionZ;
-//                    outputcloud3 = ConvertCloudtoVector(segmented_cloud);
-//                }
-//                else if (number == 3)
-//                {
-//                    x4 += dimensionX;
-//                    y4 += dimensionY;
-//                    z4 += dimensionZ;
-//                    outputcloud4 = ConvertCloudtoVector(segmented_cloud);
-//                }
-//                else if (number == 4)
-//                {
-//                    x5 += dimensionX;
-//                    y5 += dimensionY;
-//                    z5 += dimensionZ;
-//                    outputcloud5 = ConvertCloudtoVector(segmented_cloud);
-//                }
-//            }
+                if (number == 0)
+                {
+                    x1 += dimensionX;
+                    y1 += dimensionY;
+                    z1 += dimensionZ;
+                    outputcloud1 = ConvertCloudtoVector(segmented_cloud);
+                }
+                else if (number == 1)
+                {
+                    x2 += dimensionX;
+                    y2 += dimensionY;
+                    z2 += dimensionZ;
+                    outputcloud2 = ConvertCloudtoVector(segmented_cloud);
+                }
+                else if (number == 2)
+                {
+                    x3 += dimensionX;
+                    y3 += dimensionY;
+                    z3 += dimensionZ;
+                    outputcloud3 = ConvertCloudtoVector(segmented_cloud);
+                }
+                else if (number == 3)
+                {
+                    x4 += dimensionX;
+                    y4 += dimensionY;
+                    z4 += dimensionZ;
+                    outputcloud4 = ConvertCloudtoVector(segmented_cloud);
+                }
+                else if (number == 4)
+                {
+                    x5 += dimensionX;
+                    y5 += dimensionY;
+                    z5 += dimensionZ;
+                    outputcloud5 = ConvertCloudtoVector(segmented_cloud);
+                }
+            }
         }
 
         outputdata.input = ConvertCloudtoVector(cloudnew);
-//        outputdata.dimensions1.push_back(x1/10);
-//        outputdata.dimensions1.push_back(y1/10);
-//        outputdata.dimensions1.push_back(z1/10);
+        outputdata.dimensions1.push_back(x1/10);
+        outputdata.dimensions1.push_back(y1/10);
+        outputdata.dimensions1.push_back(z1/10);
 
-//        outputdata.dimensions2.push_back(x2/10);
-//        outputdata.dimensions2.push_back(y2/10);
-//        outputdata.dimensions2.push_back(z2/10);
+        outputdata.dimensions2.push_back(x2/10);
+        outputdata.dimensions2.push_back(y2/10);
+        outputdata.dimensions2.push_back(z2/10);
 
-//        outputdata.dimensions3.push_back(x3/10);
-//        outputdata.dimensions3.push_back(y3/10);
-//        outputdata.dimensions3.push_back(z3/10);
+        outputdata.dimensions3.push_back(x3/10);
+        outputdata.dimensions3.push_back(y3/10);
+        outputdata.dimensions3.push_back(z3/10);
 
-//        outputdata.dimensions4.push_back(x4/10);
-//        outputdata.dimensions4.push_back(y4/10);
-//        outputdata.dimensions4.push_back(z4/10);
+        outputdata.dimensions4.push_back(x4/10);
+        outputdata.dimensions4.push_back(y4/10);
+        outputdata.dimensions4.push_back(z4/10);
 
-//        outputdata.dimensions5.push_back(x5/10);
-//        outputdata.dimensions5.push_back(y5/10);
-//        outputdata.dimensions5.push_back(z5/10);
+        outputdata.dimensions5.push_back(x5/10);
+        outputdata.dimensions5.push_back(y5/10);
+        outputdata.dimensions5.push_back(z5/10);
 
-        outputdata.dimensions1.push_back(10);
-        outputdata.dimensions1.push_back(10);
-        outputdata.dimensions1.push_back(10);
+//        outputdata.dimensions1.push_back(10);
+//        outputdata.dimensions1.push_back(10);
+//        outputdata.dimensions1.push_back(10);
 
-        outputdata.dimensions2.push_back(10);
-        outputdata.dimensions2.push_back(10);
-        outputdata.dimensions2.push_back(10);
+//        outputdata.dimensions2.push_back(10);
+//        outputdata.dimensions2.push_back(10);
+//        outputdata.dimensions2.push_back(10);
 
-        outputdata.dimensions3.push_back(10);
-        outputdata.dimensions3.push_back(10);
-        outputdata.dimensions3.push_back(10);
+//        outputdata.dimensions3.push_back(10);
+//        outputdata.dimensions3.push_back(10);
+//        outputdata.dimensions3.push_back(10);
 
-        outputdata.dimensions4.push_back(10);
-        outputdata.dimensions4.push_back(10);
-        outputdata.dimensions4.push_back(10);
+//        outputdata.dimensions4.push_back(10);
+//        outputdata.dimensions4.push_back(10);
+//        outputdata.dimensions4.push_back(10);
 
-        outputdata.dimensions5.push_back(10);
-        outputdata.dimensions5.push_back(10);
-        outputdata.dimensions5.push_back(10);
+//        outputdata.dimensions5.push_back(10);
+//        outputdata.dimensions5.push_back(10);
+//        outputdata.dimensions5.push_back(10);
 
-        outputdata.box1.push_back(0);
-        outputdata.box2.push_back(0);
-        outputdata.box3.push_back(0);
-        outputdata.box4.push_back(0);
-        outputdata.box5.push_back(0);
+//        outputdata.box1.push_back(0);
+//        outputdata.box2.push_back(0);
+//        outputdata.box3.push_back(0);
+//        outputdata.box4.push_back(0);
+//        outputdata.box5.push_back(0);
 
-//        outputdata.box1 = outputcloud1;
-//        outputdata.box2 = outputcloud2;
-//        outputdata.box3 = outputcloud3;
-//        outputdata.box4 = outputcloud4;
-//        outputdata.box5 = outputcloud5;
+        outputdata.box1 = outputcloud1;
+        outputdata.box2 = outputcloud2;
+        outputdata.box3 = outputcloud3;
+        outputdata.box4 = outputcloud4;
+        outputdata.box5 = outputcloud5;
 
         return outputdata;
     }
@@ -168,7 +167,7 @@ std::vector<short> PCLViewer::ConvertCloudtoVector(pcl::PointCloud<pcl::PointXYZ
 
          ++j;
      }
-//     points.resize(j*3);
+
      return points;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
