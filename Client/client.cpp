b3507df6ef48d3bb0a32065b374a127bd45acc2a
rodrigo <rodrigo@debian.debian>
2021-10-22 14:31:07 -0300

    cleaning code

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,32 +1,12 @@
-//
-// client.cpp
-// ~~~~~~~~~~
-//
-// Copyright (c) 2003-2016 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
 #include <iostream>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
-
-#include <boost/bind.hpp>
-#include <iostream>
 #include <vector>
-#include "../connection.hpp" // Must come before boost/serialization headers.
 #include <boost/serialization/vector.hpp>
-#include <boost/thread.hpp>
 #include <boost/serialization/serialization.hpp>
-#include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 
-
 using boost::asio::ip::tcp;
-static boost::mutex mx;
-
-
 
 struct ObjectsData {
 
@@ -59,114 +39,6 @@ struct ObjectsData {
 };
 
 
-//namespace s11n_example {
-
-///// Downloads stock quote information from a server.
-//class client
-//{
-//public:
-//  /// Constructor starts the asynchronous connect operation.
-//  client(boost::asio::io_service& io_service, const std::string& host, const std::string& service) : connection_(io_service)
-//  {
-//    // Resolve the host name into an IP address.
-//    boost::asio::ip::tcp::resolver resolver(io_service);
-//    boost::asio::ip::tcp::resolver::query query(host, service);
-//    boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
-//    // Start an asynchronous connect operation.
-//    boost::asio::async_connect(connection_.socket(), endpoint_iterator, boost::bind(&client::handle_connect, this, boost::asio::placeholders::error));
-//  }
-
-//  /// Handle completion of a connect operation.
-//  void handle_connect(const boost::system::error_code& e)
-//  {
-//    if (!e)
-//    {
-//      // Successfully established connection. Start operation to read the list
-//      // of stocks. The connection::async_read() function will automatically
-//      // decode the data that is read from the underlying socket.
-//      connection_.async_read(readdata, boost::bind(&client::handle_read, this, boost::asio::placeholders::error));
-//    }
-//    else
-//    {
-//      // An error occurred. Log it and return. Since we are not starting a new
-//      // operation the io_service will run out of work to do and the client will
-//      // exit.
-//      std::cerr << e.message() << std::endl;
-//    }
-//  }
-
-//  /// Handle completion of a read operation.
-//  void handle_read(const boost::system::error_code& e)
-//  {
-//    if (!e)
-//    {
-//      // Print out the data that was received.
-//      for (std::size_t i = 0; i < readdata.size(); ++i)
-//      {
-//        std::cout << "Data number " << i+1 << "\n";
-//        std::cout << "  Input cloud size: " << readdata[i].input.size() << "\n";
-//        std::cout << "  Object 1 cloud size: " << readdata[i].box1.size() << "\n";
-//        std::cout << "  Object 1 dimensions: X: " << readdata[i].dimensions1[0] << "; Y: " << readdata[i].dimensions1[1] << "; Z: " << readdata[i].dimensions1[2] << "\n";
-//        std::cout << "  Object 2 cloud size: " << readdata[i].box2.size() << "\n";
-//        std::cout << "  Object 2 dimensions: X: " << readdata[i].dimensions2[0] << "; Y: " << readdata[i].dimensions2[1] << "; Z: " << readdata[i].dimensions2[2] << "\n";
-//        std::cout << "  Object 3 cloud size: " << readdata[i].box3.size() << "\n";
-//        std::cout << "  Object 3 dimensions: X: " << readdata[i].dimensions3[0] << "; Y: " << readdata[i].dimensions3[1] << "; Z: " << readdata[i].dimensions3[2] << "\n";
-//        std::cout << "  Object 4 cloud size: " << readdata[i].box4.size() << "\n";
-//        std::cout << "  Object 4 dimensions: X: " << readdata[i].dimensions4[0] << "; Y: " << readdata[i].dimensions4[1] << "; Z: " << readdata[i].dimensions4[2] << "\n";
-//        std::cout << "  Object 5 cloud size: " << readdata[i].box5.size() << "\n";
-//        std::cout << "  Object 5 dimensions: X: " << readdata[i].dimensions5[0] << "; Y: " << readdata[i].dimensions5[1] << "; Z: " << readdata[i].dimensions5[2] << "\n";
-//      }
-//    }
-//    else
-//    {
-//      // An error occurred.
-//      std::cerr << e.message() << std::endl;
-//    }
-
-//    // Since we are not starting a new operation the io_service will run out of
-//    // work to do and the client will exit.
-//  }
-
-//private:
-//  /// The connection to the server.
-//  connection connection_;
-
-//  /// The data received from the server.
-//  std::vector<ObjectsData> readdata;
-//};
-
-//} // namespace s11n_example
-
-//int main(int argc, char* argv[])
-//{
-//  try
-//  {
-//    // Check command line arguments.
-//    if (argc != 2)
-//    {
-//      std::cerr << "Usage: client <host> " << std::endl;
-//      return 1;
-//    }
-
-//    boost::asio::io_service io_service;
-
-//    s11n_example::client client(io_service, argv[1], "13");
-//    io_service.run();
-
-//  }
-//  catch (std::exception& e)
-//  {
-//    std::cerr << e.what() << std::endl;
-//  }
-
-//  return 0;
-//}
-
-
-
-
-
 int main(int argc, char* argv[])
 {
   try
@@ -188,16 +60,11 @@ int main(int argc, char* argv[])
 
     for (;;)
     {
-      ObjectsData readdata;
-     boost::asio::streambuf buf;
-      boost::system::error_code error;
+        ObjectsData readdata;
+        boost::asio::streambuf buf;
+        boost::system::error_code error;
 
-
-//      size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-      std::size_t length = boost::asio::read(socket, buf, error);
-//        size_t len = socket.read_some(boost::asio::buffer(buf), error);
-//      boost::lock_guard<boost::mutex> lk(mx);
+        std::size_t length = boost::asio::read(socket, buf, error);
 
         std::istream is(&buf);
         boost::archive::binary_iarchive ia(is);
@@ -205,12 +72,21 @@ int main(int argc, char* argv[])
         ia >> readdata;
 
         std::cout << "  Input cloud size: " << readdata.input.size() << "\n";
+        std::cout << "  Object 1 cloud size: " << readdata.box1.size() << "\n";
+        std::cout << "  Object 1 dimensions: X: " << readdata.dimensions1[0] << "; Y: " << readdata.dimensions1[1] << "; Z: " << readdata.dimensions1[2] << "\n";
+        std::cout << "  Object 2 cloud size: " << readdata.box2.size() << "\n";
+        std::cout << "  Object 2 dimensions: X: " << readdata.dimensions2[0] << "; Y: " << readdata.dimensions2[1] << "; Z: " << readdata.dimensions2[2] << "\n";
+        std::cout << "  Object 3 cloud size: " << readdata.box3.size() << "\n";
+        std::cout << "  Object 3 dimensions: X: " << readdata.dimensions3[0] << "; Y: " << readdata.dimensions3[1] << "; Z: " << readdata.dimensions3[2] << "\n";
+        std::cout << "  Object 4 cloud size: " << readdata.box4.size() << "\n";
+        std::cout << "  Object 4 dimensions: X: " << readdata.dimensions4[0] << "; Y: " << readdata.dimensions4[1] << "; Z: " << readdata.dimensions4[2] << "\n";
+        std::cout << "  Object 5 cloud size: " << readdata.box5.size() << "\n";
+        std::cout << "  Object 5 dimensions: X: " << readdata.dimensions5[0] << "; Y: " << readdata.dimensions5[1] << "; Z: " << readdata.dimensions5[2] << "\n";
 
-      if (error == boost::asio::error::eof)
-        break; // Connection closed cleanly by peer.
-      else if (error)
-        throw boost::system::system_error(error); // Some other error.
-
+        if (error == boost::asio::error::eof)
+            break; // Connection closed cleanly by peer.
+        else if (error)
+            throw boost::system::system_error(error); // Some other error.
     }
   }
   catch (std::exception& e)
