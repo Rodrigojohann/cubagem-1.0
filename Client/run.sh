e7036bb34ad3fff43039739f8e4a4c902f37d183
rodrigo <rodrigo@debian.debian>
2021-11-09 10:15:35 -0300

    docker run modified

--- Client/run.sh
+++ Client/run.sh
@@ -1,2 +1,2 @@
 docker build . -t cubagem-client:1.0
-docker run -it --network=home_net cubagem-client:1.0
+docker run -it --network=home_net --name cubagem-client cubagem-client:1.0
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker build -t cubagem-server:1.0 .
 docker network create –-driver bridge home_net
-docker run -p 13:13 -ti --network=home_net cubagem-server:1.0
+docker run -p 13:13 -ti --network=home_net --name cubagem-server --ip 172.18.0.5 cubagem-server:1.0
 
